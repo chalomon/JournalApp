8456ef1d031c2bedc307644782bc01a37d5dc63e
soporte4 <soporte4@imet.local>
2022-11-04 16:30:29 -0300

    Avance de la máscara de la página

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,24 +1,13 @@
 import { Link as RouterLink } from 'react-router-dom'
 import { Google } from "@mui/icons-material"
 import { Grid, Typography, TextField, Button, Link } from "@mui/material"
+import { AuthLayout } from '../layout/AuthLayout'
 
 
 export const LoginPage = () => {
   return (
-    <Grid 
-      container
-      spacing={0}
-      direction="column"
-      alignItems="center"
-      justifyContent="center"
-      sx={{minHeight: '100vh', backgroundColor: 'primary.main', padding: 4}}
-    >
-      <Grid item
-        className='box-shadow'
-        xs={3}
-        sx={{backgroundColor: 'white', padding: 3, borderRadius: 2}}
-      >
-        <Typography variant='h5' sx={{mb:1}}>Login</Typography>
+    
+    <AuthLayout title='Login'>
 
         <form>
           <Grid container>
@@ -68,9 +57,8 @@ export const LoginPage = () => {
 
 
         </form>
+    </AuthLayout>
 
-      </Grid>
 
-    </Grid>
   )
 }
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,10 +1,14 @@
 import { MailOutline } from "@mui/icons-material"
 import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant="h1">JournalPage</Typography>
-    </>
+    <JournalLayout>
+      <Typography>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae saepe magni voluptatibus vitae, fuga qui! Mollitia pariatur repudiandae laborum asperiores, temporibus iusto, eveniet culpa non tempora qui, dicta ab unde?
+      </Typography>
+    </JournalLayout>
+    
   )
 }
