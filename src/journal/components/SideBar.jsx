657d57ad057823e60881f38dc31143e130297ccf
Gonzalo Pacheco <gonzalopachecozamorano@gmail.com>
2023-09-05 16:08:12 -0300

    Prueba de responsive app

--- src/journal/components/SideBar.jsx
+++ src/journal/components/SideBar.jsx
@@ -1,27 +1,23 @@
 import { Box, Divider, Drawer, List, Toolbar, Typography } from "@mui/material"
 import { useSelector } from "react-redux";
 import { SideBarItem } from "./";
+import { useState } from "react";
 
-export const SideBar = ({drawerWidth = 240}) => {
+
+
+export const SideBar = (props, {drawerWidth = 240}) => {
 
     const { displayName } = useSelector( state => state.auth );
     const { notes } = useSelector( state => state.journal );
 
-    
+    const [mobileOpen, setMobileOpen] = useState(false);
 
-  return (
-    <Box
-        component='nav'
-        sx={{width: {sm: drawerWidth}, flexShrink: {sm:0} }}
-    >
-        <Drawer
-            variant='permanent' //temporary si es que quiere ocultarse
-            open={true}
-            sx={{
-                display: {xs: 'block'},
-                '& .MuiDrawer-paper': {boxSizing: 'border-box', width: drawerWidth}
-            }}
-        >
+    const handleDrawerToggle = () => {
+        setMobileOpen(!mobileOpen);
+      };
+
+      const drawer = (
+        <>
             <Toolbar>
                 <Typography variant='h6' noWrap component='div'>
                     {displayName}
@@ -38,7 +34,42 @@ export const SideBar = ({drawerWidth = 240}) => {
                     ))
                 }
             </List>
+        </>
+      );
 
+      const { window } = props;
+
+      const container = window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <Box
+        component='nav'
+        sx={{width: {sm: drawerWidth}, flexShrink: {sm:0} }}
+    >
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+        >
+          {drawer}
+        </Drawer>
+        <Drawer
+          variant="permanent"
+          sx={{
+            display: { xs: 'none', sm: 'block' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+          open
+        >
+          {drawer}
         </Drawer>
 
     </Box>
